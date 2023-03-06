f2cb87bb05c67324612c12be4a7ed258e8c232a7
FortuneFire <fmlilo15@outlook.com>
2023-03-06 05:45:09 +0200

    Form now working. categories and skills checkbox fixed. pictures upload outstanding

--- src/app/components/projects/project-form.component.ts
+++ src/app/components/projects/project-form.component.ts
@@ -1,5 +1,3 @@
-
-
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
@@ -8,144 +6,52 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './project-form.component.html',
   styleUrls: ['./project-form.component.css']
 })
-export class ProjectFormComponent implements OnInit{
+export class ProjectFormComponent implements OnInit {
   projectForm!: FormGroup;
-  categories = [{name:'Web Development', value:'Web Development'}, 
-                {name:'Web Design', value:'Web Design'}, 
-                {name:'Graphic Design', value: 'Graphic Design'}
-              ];
+  categories = ['Web Development', 'Web Design', 'Graphic Design'];
+  skills = ['Angular', 'CSS', 'HTML', 'JS', 'Bootstrap', 'AdobeXD'];
 
-  skills = [ {name:'Angular', value:'Angular'}, 
-              {name:'CSS', value:'CSS'}, 
-              {name:'HTML', value:'HTML'}, 
-              {name:'JS', value:'JS'}, 
-              {name:'Boostrap', value:'Boostrap'}, 
-              {name:'AdobeXD', value:'AdobeXD'}, 
-              {name:'Photoshop', value:'Photoshop'}, 
-              {name:'Illustrator', value:'Illustrator'}, 
-              {name:'Wordpress', value:'Wordpress'}
-            ];
-  keyImageFile!: File;
-  galleryImageFiles: File[] = [];
-  keyImageSrc: string | null = null;
-  galleryImageSrcs: string[] = [];
-  // showLabels = false;
+  constructor(private formBuilder: FormBuilder) { }
 
-  constructor(private fb: FormBuilder) {
-    
-      this.projectForm = this.fb.group({
-        title: ['', Validators.required],
-        highlight: ['', Validators.required],
-        description: ['', Validators.required],
-        category: this.addCategoryCheckboxes(),
-        skills: this.addSkillsCheckboxes(),
-        keyIMG: ['', Validators.required],
-        gallery: [[]]
-      });
-   
+  ngOnInit() {
+    this.projectForm = this.formBuilder.group({
+      title: '',
+      highlight: '',
+      description: '',
+      categories: this.formBuilder.array([]),
+      skills: this.formBuilder.array([])
+    });
+    this.addCheckboxes();
+  }
+
+  private addCheckboxes() {
+    this.categories.forEach(() => this.categoriesFormArray.push(this.formBuilder.control(false)));
+    this.skills.forEach(() => this.skillsFormArray.push(this.formBuilder.control(false)));
+  }
+
+  get categoriesFormArray() {
+    return this.projectForm.controls['categories'] as FormArray;
   }
 
   get skillsFormArray() {
     return this.projectForm.controls['skills'] as FormArray;
   }
 
-  get categoryFormArray() {
-    return this.projectForm.controls['category'] as FormArray;
-  }
-  
-
-  ngOnInit(): void {
-    // this.showLabels = true;
-  }
-
-  
-  addCategoryCheckboxes() {
-    const arr = this.categories.map(category => {
-      return this.fb.control(false);
-    });
-    return this.fb.array(arr);
-  }
-
-
-  addSkillsCheckboxes() {
-    const arr = this.skills.map(skill => {
-      return this.fb.control(skill);
-    });
-    return this.fb.array(arr);
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  onKeyImageDropped(event: any) {
-     // Get the dropped file from the event
-  const file = event.files[0];
-  
-  // Assign the file to the keyImageFile property
-  this.keyImageFile = file;
-  }
-
-  onKeyImageSelected(event: any) {
-    const file: File = event.target.files[0];
-    this.keyImageFile = file;
-    this.keyImageSrc = URL.createObjectURL(file);
-  }
-
-  onGalleryImageDropped(event: any) {
-    const files = event.files;
-  
-    // Loop through the files and add them to the galleryImageFiles array
-    for (const file of files) {
-      this.galleryImageFiles.push(file);
-    }
-  }
-
-  onGalleryImageSelected(event: any) {
-    this.galleryImageFiles = [];
-    for (let i = 0; i < event.target.files.length; i++) {
-      this.galleryImageFiles.push(event.target.files[i]);
-    }
-  }
-
-
   onSubmit() {
-        // const selectedCategories = this.projectForm.value.category
-        //   .map((checked: any, index: string | number) => checked ? this.categories[2] : null)
-        //   .filter((value: null) => value !== null);
-        // const selectedSkills = this.projectForm.value.skills
-        //   .map((checked: any, index: string | number) => checked ? this.skills[1] : null)
-        //   .filter((value: null) => value !== null);
-    
-        // const project = {
-        //   title: this.projectForm.value.title,
-        //   highlight: this.projectForm.value.highlight,
-        //   description: this.projectForm.value.description,
-        //   category: selectedCategories,
-        //   skills: selectedSkills,
-        //   keyIMG: this.keyImageFile ? URL.createObjectURL(this.keyImageFile) : '',
-        //   gallery: []
-        // };
-    
-        // for (const file of this.galleryImageFiles) {
-        //   project.gallery.push(URL.createObjectURL(file));
-        // }
-    
-        // console.log(project);
-        
-        this.projectForm.reset();
-      }
-    
-    }
+    const selectedCategories = this.categoriesFormArray.value
+      .map((checked: any, i: any) => checked ? this.categories[i] : null)
+      .filter((v: null) => v !== null);
+    const selectedSkills = this.skillsFormArray.value
+      .map((checked: any, i: any) => checked ? this.skills[i] : null)
+      .filter((v: null) => v !== null);
+
+    console.log({
+      title: this.projectForm.value.title,
+      highlight: this.projectForm.value.highlight,
+      description: this.projectForm.value.description,
+      categories: selectedCategories,
+      skills: selectedSkills
+    });
+    this.projectForm.reset();
+  }
+}
